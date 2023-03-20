1afb230bd4e3113542bf968083541cf5699b3a45
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2023-03-20 13:38:49 +0000

    java

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04{
+    public static void main(String[] args){
+        String name = "Yukino";
+        System.out.println(name);
+
+        name = "一生懸命";
+        System.out.println(name);
+
+        int num = 6;
+        System.out.println(num);
+    }
+}
