6477b763049d18042622b52b00fcc5216541ad75
EC2 Default User <ec2-user@ip-172-31-87-77.ec2.internal>
2023-03-21 02:36:40 +0000

    java03211136

---
+++ Chapter05.java
@@ -0,0 +1,21 @@
+public class Chapter05{
+    public static void main(String[] args){
+        long l = 10000000000L;
+        System.out.println(l);
+        double d = 9.52;
+        System.out.println(d);
+        boolean b = true;
+        System.out.println(b);
+
+        Float f = 1.4142f;
+        System.out.println(f);
+        Integer i = Integer.valueOf(15);
+        System.out.println(i);
+        Character c = 'う';
+        System.out.println(c);
+
+        int[] array = {0, 10, 20, 30, 100};
+        System.out.println(array[4]);
+
+    }
+}
